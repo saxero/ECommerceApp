65b0f8a07b90fa35c4523b2c5ae3887aeb87cc77
saxero <jvives@gmail.com>
2017-11-19 15:46:57 +0100

    ECommerce first load: MasterDetail Page + colors

---
+++ ShoppingCart/ShoppingCart/ShoppingCart/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ShoppingCart.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ShoppingCart
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(MainViewModel vm)
+        {
+            this.BindingContext = vm;
+            InitializeComponent();
+        }
+    }
+}
